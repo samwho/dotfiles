96c0e8610c18713e9e0b7f2a0048a44dc000dc52
Sam Rose <samwho@lbak.co.uk>
2017-04-16 02:29:00 +0100

    Changed PS1 to include host and user, also added more stuff to tmux.conf

--- .bashrc.d/ps1.bash
+++ .bashrc.d/ps1.bash
@@ -1,1 +1,20 @@
-export PS1="\[\033[38;5;247m\][\[$(tput sgr0)\]\[\033[38;5;2m\]\A\[$(tput sgr0)\]\[\033[38;5;247m\]]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;7m\]\W\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;124m\]\$?\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;247m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
+BASE03=$(tput setaf 234)
+BASE02=$(tput setaf 235)
+BASE01=$(tput setaf 240)
+BASE00=$(tput setaf 241)
+BASE0=$(tput setaf 244)
+BASE1=$(tput setaf 245)
+BASE2=$(tput setaf 254)
+BASE3=$(tput setaf 230)
+YELLOW=$(tput setaf 136)
+ORANGE=$(tput setaf 166)
+RED=$(tput setaf 160)
+MAGENTA=$(tput setaf 125)
+VIOLET=$(tput setaf 61)
+BLUE=$(tput setaf 33)
+CYAN=$(tput setaf 37)
+GREEN=$(tput setaf 64)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+PS1="\[${BOLD}${BLUE}\]\u${RESET}@\[${BOLD}${YELLOW}\h\]\[${BOLD}${BASE0}\]:\[${BOLD}${BASE1}\]\w\[${RESET}\] \$ "
