4b25f3ae8b9449af97ba4c9da31d5021c2d8face
Sam Rose <samwho@lbak.co.uk>
2017-09-09 20:12:31 +0100

    Sync changes made at work with personal branch.

---
+++ .bashrc.d/aliases.d/ssh.bash
@@ -0,0 +1,2 @@
+alias ubiquity="ssh samwho-ubiquity.fra.corp.google.com"
+alias workstation="ssh merc.lon.corp.google.com"
--- .bashrc.d/base.bash
+++ .bashrc.d/base.bash
@@ -2,3 +2,19 @@
 [ -z "$TMUX" ] && export TERM=xterm-256color
 
 export PATH=$HOME/.bin:$PATH
+
+# Stolen/adapted from: https://unix.stackexchange.com/a/22215
+findUp() {
+    path=$(pwd)
+    while [[ "$path" != "" && ! -e "$path/$1" ]]; do
+        path=${path%/*}
+    done
+
+    if [ "$path" == "" ]
+    then
+        return 1
+    fi
+
+    echo "$path/$1"
+}
+
