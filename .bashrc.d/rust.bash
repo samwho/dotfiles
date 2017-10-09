9a93b1d8d48f00febb343625f646e0986f897a36
Sam Rose <samwho@lbak.co.uk>
2017-10-09 23:12:10 +0100

    Fix Rust source path

--- .bashrc.d/rust.bash
+++ .bashrc.d/rust.bash
@@ -2,4 +2,4 @@ if [ -f $HOME/.cargo/env ]; then
         source $HOME/.cargo/env
 fi
 
-export RUST_SRC_PATH="$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
+export RUST_SRC_PATH=("$HOME/.rustup/toolchains/stable-*/lib/rustlib/src/rust/src")
